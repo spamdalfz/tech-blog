22e03dbcd0ca6f5efefcca3b33474799a56850b6
spamdalfz <coreyvasser@gmail.com>
2023-05-01 17:27:11 -0700

    intial dependencies and server setup

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const hbs = exphbs.create({ /* ... */ });
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  store: new SequelizeStore({ db: sequelize }),
+  resave: false,
+  saveUninitialized: true,
+}));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+});
